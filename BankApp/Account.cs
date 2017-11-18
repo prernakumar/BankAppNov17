7c998ddcf375f1d72f1708a5d3e5a676a44e0ef5
Prerna <prerna16.k@gmail.com>
2017-11-18 03:15:45 -0800

    Add methods and create an object

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -10,12 +10,23 @@ namespace BankApp
     {
         #region Properties
 
-        public int AccountNumber { get; set;}
+        public int AccountNumber { get; private set;}
         public string EmailAddress { get; set;}
         public string AccountName { get; set;}
         public string AccountType { get; set; }
-        public decimal Balance { get; set; }
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
 
         #endregion
     }
+    
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,6 +10,13 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
+            var account = new Account();
+            //account.AccountNumber = 123456;
+            account.EmailAddress = "test@test.com";
+            account.AccountType = "Checking";
+            account.Deposit(100.10M);
+            Console.WriteLine($"{account.Balance}");
+
         }
     }
 }

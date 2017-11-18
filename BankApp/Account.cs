80c976069d9c870a8a9e3fed655a3a93499b8637
Prerna <prerna16.k@gmail.com>
2017-11-18 02:34:07 -0800

    initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Account
+    {
+        #region Properties
+
+        public int AccountNumber { get; set;}
+        public string EmailAddress { get; set;}
+        public string AccountName { get; set;}
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+
+        #endregion
+    }
+}
